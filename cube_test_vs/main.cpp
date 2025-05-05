26b4e0661b38cdcdb874116f04423f1fd1f1b1a5
anaselii <aninosspro@gmail.com>
2025-05-05 11:34:23 +0100

    Cleaning the Solution

--- cube_test_vs/main.cpp
+++ cube_test_vs/main.cpp
@@ -304,79 +304,6 @@ private:
 				return;
 			}
 
-		///////////// -- Load Texture -- ////////////////
-
-		////---- Load DDS
-		//RoxFormats::DirectDrawSurface ddsFile;
-		//FILE* file = nullptr;
-		//fopen_s(&file, "resources/test.dds", "rb");
-		//if (!file) {
-		//	std::cerr << "Failed to open DDS file\n";
-		//	return;
-		//}
-		//
-		//// Get file size
-		//fseek(file, 0, SEEK_END);
-		//size_t fileSize = ftell(file);
-		//fseek(file, 0, SEEK_SET);
-		//
-		//// Read file data into buffer
-		//std::vector<char> buffer(fileSize);
-		//fread(buffer.data(), 1, fileSize, file);
-		//fclose(file);
-		//
-		//// Decode the DDS header and data
-		//size_t headerSize = ddsFile.decodeHeader(buffer.data(), buffer.size());
-		//if (headerSize == 0) {
-		//	std::cerr << "Failed to decode DDS header\n";
-		//	return;
-		//}
-		//
-		//// Now you can use the decoded data to build a texture
-		//RoxRender::RoxTexture::COLOR_FORMAT format;
-		//switch (ddsFile.pf) {
-		//case RoxFormats::DirectDrawSurface::DXT1: format = RoxRender::RoxTexture::DXT1; break;
-		//case RoxFormats::DirectDrawSurface::DXT3: format = RoxRender::RoxTexture::DXT3; break;
-		//case RoxFormats::DirectDrawSurface::DXT5: format = RoxRender::RoxTexture::DXT5; break;
-		//case RoxFormats::DirectDrawSurface::RGBA: format = RoxRender::RoxTexture::COLOR_RGBA; break;
-		//case RoxFormats::DirectDrawSurface::BGRA: format = RoxRender::RoxTexture::COLOR_RGBA; break;
-		//default: std::cerr << "Unsupported DDS format" << std::endl; return;
-		//}
-		//m_texture.buildTexture(ddsFile.data, ddsFile.width, ddsFile.height, format,
-			//ddsFile.need_generate_mipmaps ? -1 : ddsFile.mipmap_count);
-
-		////---- Load TGA
-		// Create a shared texture resrouce from a TGA file
-		RoxFormats::TgaFile tgaFile;
-		if (!tgaFile.load("wall.tga")) {
-			std::cerr << "Failed to load texture!\n";
-			return;
-		}
-
-		// Get Image Data
-		const unsigned char* data = tgaFile.getData();
-		int width = tgaFile.getWidth();
-		int height = tgaFile.getHeight();
-		int Channels = tgaFile.getChannels();
-
-		RoxRender::RoxTexture::COLOR_FORMAT format;
-		if (Channels == RoxFormats::TGA::BGR) {
-			std::cout << "COLOR RGB\n";
-			format = RoxRender::RoxTexture::COLOR_RGB;
-		
-		}
-		else if (Channels == RoxFormats::TGA::BGRA) {
-			std::cout << "COLOR RGBA\n";
-			format = RoxRender::RoxTexture::COLOR_BGRA;
-		}
-		else {
-			std::cout << "COLOR Grey\n";
-			format = RoxRender::RoxTexture::GREYSCALE;
-		
-		}
-		m_texture.buildTexture(data, width, height, format);
-		////////////////////////////
-
 		///////////// -- Load Shaders -- ////////////////
 		RoxRender::RoxShader::setBinaryShaderCachingEnabled(false);
 		m_shader.addProgram(RoxRender::RoxShader::VERTEX, m_vertex_code.c_str());
@@ -386,134 +313,6 @@ private:
 			return;
 		}
 		////////////////////////////
-
-		///////////// -- Save/Load Shaders -- ////////////////
-		//RoxRender::RoxCompiledShader shader;
-		//RoxSystem::RoxShaderCacheProvider shader_cache_provider;
-		//
-		//shader_cache_provider.setLoadPath(RoxSystem::getAppPath());
-		//shader_cache_provider.setSavePath(RoxSystem::getAppPath());
-		//
-		//// Combined Both Shaders Text for saving
-		//size_t vs_length = strlen(m_vertex_code.c_str());
-		//size_t ft_length = strlen(m_fragment_code.c_str());
-		//size_t total_length = vs_length + ft_length + 1; // +1 for the null terminator
-		//
-		//char* combined = new char[total_length];
-		//strcpy_s(combined, total_length, m_vertex_code.c_str());
-		//strcat_s(combined, total_length, m_fragment_code.c_str());
-
-
-		//bool shader_exist = true ? false : shader_cache_provider.get(combined, shader);
-		//shader_exist = false;
-		//std::cout << "Shader Exist: " << (shader_exist ? "true" : "false") << "\n";
-		
-		
-		
-		//else if (!shader_exist)
-		//{
-		//	RoxRender::RoxShader::setBinaryShaderCachingEnabled(true);
-		//	m_shader.addProgram(RoxRender::RoxShader::VERTEX, m_vertex_code.c_str());
-		//	m_shader.addProgram(RoxRender::RoxShader::PIXEL, m_fragment_code.c_str());
-		//
-		//	
-		//	if (m_shader.getProgramBinaryShader(shader))
-		//	{
-		//		//std::cout << "Saving binary data to disk\n";
-		//		shader_cache_provider.set(combined, shader);
-		//	}
-		//}
-		//else
-		//{
-		//	if (shader_cache_provider.get(combined, shader))
-		//	{
-		//		std::cout << "Load binary data to disk\n";
-		//		if (!m_shader.setProgramBinaryShader(m_vertex_code.c_str(), m_fragment_code.c_str(), shader))
-		//			std::cout << "Error loading binary data";
-		//	}
-		//	
-		//}
-		////////////////////////////
-
-		///////////// -- Set Uniforms -- ////////////////
-		std::cout << "============= Set Uniforms Data ===============\n";
-
-		// After successfully loading and compiling the shaders
-		int lightPosUniform = m_shader.findUniform("lightPos");
-		if (lightPosUniform >= 0) {
-			m_shader.setUniform(lightPosUniform, 1.0f, 1.0f, 2.0f);
-			std::cout << "lightPos uniform found at index " << lightPosUniform << std::endl;
-		}
-		else {
-			std::cout << "lightPos uniform not found!" << std::endl;
-		}
-
-		int lightColorUniform = m_shader.findUniform("lightColor");
-		if (lightColorUniform >= 0){ 
-			m_shader.setUniform(lightColorUniform, 1.0f, 1.0f, 1.0f);
-			std::cout << "lightColor uniform found at index " << lightColorUniform << std::endl;
-		}
-		else {
-			std::cout << "lightColor uniform not found!" << std::endl;
-		}
-		
-		int ambientStrengthUniform = m_shader.findUniform("ambientStrength");
-		if (ambientStrengthUniform >= 0){
-			m_shader.setUniform(ambientStrengthUniform, 0.2f);
-			std::cout << "ambientStrength uniform found at index " << ambientStrengthUniform << std::endl;
-		}
-		else {
-			std::cout << "ambientStrength uniform not found!" << std::endl;
-		}
-
-		std::cout << "============= Uniforms list ===============\n";
-
-		int uniform_count = m_shader.getUniformsCount();
-		std::cout << "Found " << uniform_count << " uniforms:" << std::endl;
-		for (int i = 0; i < uniform_count; ++i) {
-			std::string name = m_shader.getUniformName(i);
-			int type = m_shader.getUniformType(i);
-			std::cout << "  Uniform " << i << ": name: " << name << ", type: " << type << std::endl;
-		}
-
-		//m_shader.setUniform(0, 1.0f, 0.0f, 0.0f, 1.0f);
-		//int uniform_count = m_shader.getUniformsCount();
-		//for (int i = 0; i < uniform_count; ++i) {
-		//	std::string name = m_shader.getUniformName(i);
-		//	int type = m_shader.getUniformType(i);
-		//
-		//	// Check if it's a sampler type (7 is UNIFORM_SAMPLER2D in most implementations)
-		//	if ((name == "ourTex") && (type == 7 || type == 8)) {
-		//		m_shader.setUniform(i, 0);  // Set texture unit 0
-		//		break;
-		//	}
-		//}
-
-		//if (uniform_count > 0) {
-		//	for (int i = 0; i < uniform_count; ++i)
-		//		std::cout << "id: " << i << " Uniform name: " << m_shader.getUniformName(i) << std::endl;
-		//	m_shader.setUniform(0, 0);
-		//}
-		
-		//m_shader.setUniform(0, 1.0f, 0.0f, 0.0f, 1.0f);
-
-		//RoxRender::Rectangle viewportInfo = RoxRender::getViewport();
-		//std::cout << "x:" << viewportInfo.x << " y: " << viewportInfo.y << " width: " << viewportInfo.width << " height: " << viewportInfo.height << "\n";
-		//std::cout << "App Path: " << RoxSystem::getAppPath()<< std::endl;
-		//std::cout << "User Path: " << RoxSystem::getUserPath() << std::endl;
-		//std::cout << "Statistics state: " << RoxRender::Statistics::enabled() << "\n";
-	
-		///////////// -- Set Debugging -- ////////////////
-		RoxMath::Vector3 line_start = RoxMath::Vector3(0, 0, 0);
-		RoxMath::Vector3 line_end = RoxMath::Vector3(1, 1, 1);
-		m_debug_draw.addLine(line_start, line_end, RoxMath::Vector4(1, 0, 0, 1));
-		m_debug_draw.addTri(RoxMath::Vector3(-0.5f, -0.5f, 0.5f), RoxMath::Vector3(0.5f, -0.5f, 0.5f)
-			, RoxMath::Vector3(0.5f, 0.5f, 0.5f), RoxMath::Vector4(1, 0, 0, 1));
-
-		m_debug_draw.addQuad(RoxMath::Vector3(-1, -1, 1), RoxMath::Vector3(1, -1, 1)
-			, RoxMath::Vector3(1, 1, 1), RoxMath::Vector3(-1, 1, 1) , RoxMath::Vector4(1, 0, 0, 0.5f));
-		//m_debug_draw.addPoint(RoxMath::Vector3(0.5f, 0.5f, 0.5f));
-		//m_debug_draw.setPointSize(0.5f);
 
 	}
 
@@ -533,34 +332,11 @@ private:
 		m_camera.set_pos(mv.get_pos());
 		m_camera.set_rot(mv.get_rot());
 		
-		//m_camera.set_pos(m_move_x, m_move_y, m_move_z);
-		//m_camera.set_rot(m_rotate_x, m_rotate_y, m_rotate_z);
-		//RoxRender::setModelviewMatrix(m_camera.get_view_matrix());
-
 		RoxRender::setModelViewMatrix(mv);
-		//RoxRender::setProjectionMatrix(mv);
-		//RoxRender::setModelViewMatrix(m_camera.get_view_matrix());
 
-		//RoxRender::setCamera(m_camera.get_view_matrix());
-		// Make light move in a circle
-		//float lightX = sin(m_rot * 0.01f) * 2.0f;
-		//float lightZ = cos(m_rot * 0.01f) * 2.0f;
-
-
-		// Update light position
-		//int lightPosUniform = m_shader.findUniform("lightPos");
-		//if (lightPosUniform >= 0) {
-		//	m_shader.setUniform(lightPosUniform, lightX, 1.0f, lightZ);
-		//}
 
 		m_shader.bind();
-		//m_vbo.bind();
-		//m_texture.bind(0);
-		//m_vbo.draw();
-		//m_debug_draw.draw();
 		m_mesh.draw();
-		//m_texture.unbind(0);
-		//m_vbo.unbind();
 		m_shader.unbind();
 
 		//m_mesh.draw();
